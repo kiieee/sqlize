7cda6aadf75023d12df39ab4babea7a921e3c5f3
kie <179924858+kiieee@users.noreply.github.com>
2024-10-31 22:59:40 +0000

    fixed deleteBook comnmand

--- src/db/controllers/deleteAllBooks.js
+++ src/db/controllers/deleteAllBooks.js
@@ -2,12 +2,9 @@ const Book = require("../models/bookModel");
  
 async function deleteAllBooks(req,res) {
     try {
-       await Book.destroy ({
-        where : {},
-        truncate : true
-       })
-
-       res.status(200).json({message: `Books have been removed!`})
+        await Book.destroy ({where: {}, truncate: true})
+               
+            res.status(200).json({message: `Books have been removed!`})
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
--- src/db/controllers/deleteBook.js
+++ src/db/controllers/deleteBook.js
@@ -2,9 +2,9 @@ const Book = require("../models/bookModel");
  
 async function deleteBook(req,res) {
     try {
-        await Book.destroy ({where: {title: req.query.title}, truncate: true})
+        await Book.destroy ({where: {title: req.query.title}, })
                
-            res.status(200).json({message: `Book ${req.body.title} has been removed!`})
+            res.status(200).json({message: `Book ${req.query.title} has been removed!`})
     } catch (error) {
         console.log(error);
         res.status(500).json(error);
